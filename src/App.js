5a71d252c5e859e25561f850608e9506e8549f82
shurintou <shurintou@gmail.com>
2023-04-14 00:54:08 +0900

    feature(ImpureComponent): to add ImpureComponent.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import SimpleCase from './components/SimpleCase/fatherComponent'
+import ImpureComponent from './components/ImpureComponent/fatherComponent'
 import React from 'react'
 
 function App() {
   return (
     <div className={'box'}>
       <SimpleCase />
+      <ImpureComponent />
     </div>
   )
 }
