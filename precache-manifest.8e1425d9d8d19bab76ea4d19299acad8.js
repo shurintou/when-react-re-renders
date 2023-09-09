a058e1a767152f5404c744226c6b62a88b7c99ad
shurintou <shurintou@gmail.com>
2023-09-10 01:23:40 +0900

    initial commit.

---
+++ precache-manifest.8e1425d9d8d19bab76ea4d19299acad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f1f9d21292acf12f79f",
+    "url": "/static/css/main.826c8ed3.chunk.css"
+  },
+  {
+    "revision": "9f1f9d21292acf12f79f",
+    "url": "/static/js/main.9f1f9d21.chunk.js"
+  },
+  {
+    "revision": "c7924f5c864804d660b0",
+    "url": "/static/js/1.c7924f5c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c53f00c94b922196ba56d4ea54d1f10a",
+    "url": "/index.html"
+  }
+];
